98fe1cad0cc040562685f6ce1539cdce40c55ee7
Joao-Arthur <joao.lothamer@gmail.com>
2020-12-26 11:23:13 -0300

    começa implementação backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,107 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+use rocket::data;
+use rocket::data::FromDataSimple;
+use rocket::http::ContentType;
+use rocket::http::Status;
+use rocket::response::content::Html;
+use rocket::Data;
+use rocket::Outcome;
+use rocket::Outcome::*;
+use rocket::Request;
+use rocket::Response;
+use rocket_contrib::json::Json;
+use std::io::Read;
+
+const LIMIT: u64 = 256;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+struct Usuario {
+    login: String,
+    senha: String,
+}
+
+impl FromDataSimple for Usuario {
+    type Error = String;
+
+    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
+        let person_ct = ContentType::new("application", "json");
+        if req.content_type() != Some(&person_ct) {
+            return Outcome::Forward(data);
+        }
+
+        let mut string = String::new();
+        if let Err(e) = data.open().take(LIMIT).read_to_string(&mut string) {
+            return Failure((Status::InternalServerError, format!("{:?}", e)));
+        }
+
+        let (login, senha) = match string.find(':') {
+            Some(i) => (string[..i].to_string(), string[(i + 1)..].to_string()),
+            None => return Failure((Status::UnprocessableEntity, "':'".into())),
+        };
+
+        Success(Usuario { login, senha })
+    }
+}
+
+#[get("/<name>/<age>", format = "json")]
+fn get_hello(name: String, age: u8) -> Json<Person> {
+    let person = Person {
+        name: name,
+        age: age,
+    };
+    return Json(person);
+}
+
+#[post("/login", format = "json", data = "<usuario>")]
+fn login(usuario: Usuario) -> Status {
+    if usuario.login == "admin" && usuario.senha == "123" {
+        Status::Ok
+    } else {
+        Status::Unauthorized
+    }
+}
+
+#[options("/login")]
+fn options_handler<'a>() -> Response<'a> {
+    Response::build()
+        .raw_header("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
+        .raw_header("Access-Control-Allow-Methods", "OPTIONS, POST")
+        .raw_header("Access-Control-Allow-Headers", "Content-Type")
+        .finalize()
+}
+
+#[catch(404)]
+fn not_found(request: &Request) -> Html<String> {
+    let html = match request.format() {
+        Some(ref mt) if !mt.is_json() && !mt.is_plain() => {
+            format!("<p>'{}' requests are not supported.</p>", mt)
+        }
+        _ => format!(
+            "<p>Sorry, '{}' is an invalid path! Try \
+                 /hello/&lt;name&gt;/&lt;age&gt; instead.</p>",
+            request.uri()
+        ),
+    };
+
+    Html(html)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![get_hello, login, options_handler])
+        .register(catchers![not_found])
+        .launch();
 }
--- frontend/src/Pages/SignIn/SignIn.tsx
+++ frontend/src/Pages/SignIn/SignIn.tsx
@@ -1,7 +1,24 @@
-const SignIn = () => (
-    <div>
-        <h1>sign in</h1>
-    </div>
-);
+const SignIn = () => {
+    fetch('http://localhost:8000/login', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({ login: 'admin', senha: '1623' }) // body data type must match "Content-Type" header
+    }).then(res => {
+        console.log(res);
+    });
+    return (
+        <div>
+            <h1>sign in</h1>
+        </div>
+    );
+};
 
 export default SignIn;
