40b1396e4b41095be43b369537ff89e9d6a49623
Joao-Arthur <joao.lothamer@gmail.com>
2021-02-06 22:22:07 -0300

    preetify

--- backend/api/responses/expired.js
+++ backend/api/responses/expired.js
@@ -21,17 +21,14 @@
  * ```
  */
 module.exports = function expired() {
-
   var req = this.req;
   var res = this.res;
 
-  sails.log.verbose('Ran custom response: res.expired()');
+  sails.log.verbose("Ran custom response: res.expired()");
 
   if (req.wantsJSON) {
-    return res.status(498).send('Token Expired/Invalid');
+    return res.status(498).send("Token Expired/Invalid");
+  } else {
+    return res.status(498).view("498");
   }
-  else {
-    return res.status(498).view('498');
-  }
-
 };
--- backend/api/responses/unauthorized.js
+++ backend/api/responses/unauthorized.js
@@ -21,23 +21,20 @@
  * ```
  */
 module.exports = function unauthorized() {
-
   var req = this.req;
   var res = this.res;
 
-  sails.log.verbose('Ran custom response: res.unauthorized()');
+  sails.log.verbose("Ran custom response: res.unauthorized()");
 
   if (req.wantsJSON) {
     return res.sendStatus(401);
   }
   // Or log them out (if necessary) and then redirect to the login page.
   else {
-
     if (req.session.userId) {
       delete req.session.userId;
     }
 
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
-
 };
