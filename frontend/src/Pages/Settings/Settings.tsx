26bb4e5c43a1426c98f2355ca8d5cd87fc10f3e0
Joao-Arthur <joao.lothamer@gmail.com>
2021-03-03 21:30:31 -0300

    WIP settings

--- frontend/src/Pages/Settings/Settings.tsx
+++ frontend/src/Pages/Settings/Settings.tsx
@@ -1,24 +1,43 @@
-import { Container } from './Settings.styles';
+import { useRef } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { StateType } from '../../Store';
+import Form from '../../Components/Core/Form';
+import Field from '../../Components/Core/Field';
+import Input from '../../Components/Core/Input';
+import { Container } from './Settings.styles';
 
 export default function Settings() {
+    const language = useRef<HTMLInputElement>(null);
+
     const logged = useSelector((state: StateType) => state.User.isLogged);
     if (!logged) return <Redirect to='/signin' />;
 
     return (
         <Container>
-            <p>language</p>
-            <p>english</p>
-            <p>portuguese</p>
-            <p>spanish</p>
-            <p>german</p>
-            <p>my account</p>
-            <p>change password</p>
-            <p>enable two factor authentication</p>
-            <p>delete my account</p>
-            <p>what info do PreciseSchedule know about you?</p>
+            <Form title='Settings' loading={false} onSubmit={() => {}}>
+                <Field title='Language' name='language'>
+                    <Input
+                        ref={language}
+                        name='language'
+                        type='text'
+                        required
+                    />
+                </Field>
+                <select value='english'>
+                    <option value='english'>english</option>
+                    <option value='portuguese'>portuguese</option>
+                    <option value='spanish'>spanish</option>
+                    <option value='german'>german</option>
+                </select>
+                <span>my account</span>
+                <button>change password</button>
+                <Field title='enable two factor authentication' name='language'>
+                    <Input ref={language} name='language' type='checkbox' />
+                </Field>
+                <button>delete my account</button>
+                <button>what info do PreciseSchedule know about you?</button>
+            </Form>
         </Container>
     );
 }
